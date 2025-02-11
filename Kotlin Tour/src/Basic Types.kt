1ebcb4a7d1e4974aef55744c55f7cf403dc26622
Max <korniykom@gmail.com>
2025-02-11 11:40:11 +0200

    feat: add Basic Types exercise

---
+++ Kotlin Tour/src/Basic Types.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var customers = 10
+
+    customers = 9
+
+    customers = customers + 3
+    customers += 2
+    customers -= 5
+    customers *= 2
+    customers /= 3
+
+    print(customers)
+
+}
