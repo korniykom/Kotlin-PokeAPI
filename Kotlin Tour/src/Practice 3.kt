f67796cc6afb40147cee4c3a8b71da3921ffb0dc
Max <korniykom@gmail.com>
2025-02-11 14:47:37 +0200

    feat: add Practice 3

---
+++ Kotlin Tour/src/Practice 3.kt
@@ -0,0 +1,9 @@
+// You have a list of “green” numbers and a list of “red” numbers. Complete the code to print how many
+// numbers there are in total.
+
+fun main() {
+    val greenNumbers = listOf(1, 4, 23)
+    val redNumbers = listOf(17, 2)
+
+    println("Total number of numbers is ${greenNumbers.count() + redNumbers.count()}")
+}
