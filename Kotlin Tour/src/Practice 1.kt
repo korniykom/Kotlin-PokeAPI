aedeba1f0d9e7c50b9f0ed41d53096cc4cbfe4c2
Max <korniykom@gmail.com>
2025-02-11 09:22:12 +0200

    feat: add Practice 1

---
+++ Kotlin Tour/src/Practice 1.kt
@@ -0,0 +1,8 @@
+//Complete the code to make the program print "Mary is 20 years old" to standard output:
+
+fun main() {
+    val name = "Mary"
+    val age = 20
+
+    print("$name is $age years old")
+}
