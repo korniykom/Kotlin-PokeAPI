331374e41e2ebaf264a2d0269d41c95232604f47
Maksym Korniiko <korniykom@gmail.com>
2025-02-11 18:16:39 +0200

    feat: add Practice 11

---
+++ Kotlin Tour/src/Practice 11.kt
@@ -0,0 +1,7 @@
+ // Write a function called circleArea that takes the radius of a circle in integer format as a parameter and outputs the area of that circle.
+ import kotlin.math.PI
+ fun main() {
+    println(circleArea(2))
+ }
+
+ fun circleArea(radius: Int) = PI * radius * radius
