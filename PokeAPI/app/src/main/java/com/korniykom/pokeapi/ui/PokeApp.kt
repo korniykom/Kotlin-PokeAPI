60b3687006628766b18ed32a23711e754e502e22
Max <korniykom@gmail.com>
2025-02-24 22:23:09 +0200

    chore: create PokeAPI app

---
+++ PokeAPI/app/src/main/java/com/korniykom/pokeapi/ui/PokeApp.kt
@@ -0,0 +1,11 @@
+package com.korniykom.pokeapi.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun PokeApp(
+    modifier: Modifier = Modifier
+) {
+
+}
